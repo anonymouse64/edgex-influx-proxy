2eb265ac37eca626d5370a44a9659fcde53f9e2b
Ian Johnson <ian.johnson@canonical.com>
2018-04-09 12:48:23 -0500

    influx: refactor influx server to use REST directly instead of MQTT
    Now, the influx server directly opens a HTTP endpoint that edgex export-distro will send data to over REST. This is useful because we don't really need to have another process running, i.e. the MQTT broker, that just acts as an intermediary between edgex and the server.

--- cmd/influxdbclientserver/main.go
+++ cmd/influxdbclientserver/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
-	"os/signal"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	"bytes"
@@ -15,30 +13,24 @@ import (
 	"net/http"
 	"os"
 
-	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/edgexfoundry/edgex-go/core/domain/models"
 	influx "github.com/influxdata/influxdb/client/v2"
 	flags "github.com/jessevdk/go-flags"
 )
 
 const (
-	edgeXCreateMQTTRegistrationJSON = `{
+	edgeXCreateRESTRegistrationJSON = `{
 	"name":"golang-server",
 	"enable":true,
 	"format":"JSON",
-	"destination": "MQTT_TOPIC",
+	"destination": "REST_ENDPOINT",
 	"addressable": {
-	    "id": null,
-	    "created": 0,
-	    "modified": 0,
-	    "origin": 0,
-	    "name": "DesktopMQTT",
-	    "protocol": "TCP",
+	    "name": "DesktopREST",
+	    "protocol": "HTTP",
+   	    "method": "POST", 
 	    "address": "%s",
 	    "port": %d,
-	    "path": null,
-	    "publisher": "EdgeXExportPublisher",
-	    "topic": "EdgeXDataTopic"
+	    "path": "/edgex"
 	  }
 }`
 	httpUnknownError = iota
@@ -77,44 +69,28 @@ type StartCmd struct {
 	// general opts
 	Verbose bool `short:"v" long:"verbose" description:"Verbose output"`
 
-	// mqtt opts
-	MQTTPort       uint   `short:"m" long:"mqtt-port" description:"MQTT server port to connect to" default:"1883"`
-	MQTTSSL        bool   `short:"s" long:"mqtt-ssl" description:"MQTT connection protocol (default no encryption)"`
-	MQTTHost       string `short:"b" long:"mqtt-broker" description:"MQTT server hostname to connect to" default:"localhost"`
-	MQTTClientName string `short:"c" long:"mqtt-client" description:"MQTT clientname to use (default is automatically generated)"`
-	MQTTTopic      string `short:"t" long:"mqtt-topic" description:"MQTT topic name to subscribe on" default:"EdgeXDataTopic"`
-	MQTTUsername   string `short:"u" long:"mqtt-user" description:"MQTT server username"`
-	MQTTPassword   string `short:"p" long:"mqtt-passwd" description:"MQTT server password"`
-	MQTTQoS        int    `short:"q" long:"mqtt-qos" choice:"0" choice:"1" choice:"2" description:"MQTT Quality Of Service for the topic"`
-	MQTTSCertAuth  string `short:"i" long:"mqtt-cert" description:"MQTT secure certificate file"`
-
 	// edgex opts
-	EdgeXRegisterMQTT       bool   `short:"r" long:"edgex-export-distro-mqtt" description:"Enable Edgex Export Distro registration"`
-	EdgexDeleteRegistration bool   `short:"d" long:"edgex-export-distro-mqtt-clean" description:"Edgex Export Distro clean registration (delete existing registration before registering new one)"`
-	EdgeXExportDistroHost   string `short:"e" long:"edgex-export-distro-host" description:"Edgex Export Distro hostname (for registering the MQTT server)"`
+	EdgeXRegisterRESTClient bool   `short:"r" long:"edgex-export-distro-rest" description:"Enable Edgex Export Distro registration"`
+	EdgexDeleteRegistration bool   `short:"d" long:"edgex-export-distro-rest-clean" description:"Edgex Export Distro clean registration (delete existing registration before registering new one)"`
+	EdgeXExportDistroHost   string `short:"e" long:"edgex-export-distro-host" description:"Edgex Export Distro hostname (for registering the REST client)"`
 	EdgeXExportDistroPort   uint   `short:"f" long:"edgex-export-distro-port" description:"Edgex Export Distro port" default:"48071"`
 
 	// http opts
 	HTTPPort uint   `short:"h" long:"http-port" description:"HTTP server port to bind on" default:"8080"`
-	HTTPHost string `short:"a" long:"http-host" description:"HTTP server hostname to bind on" default:"0.0.0.0"`
+	HTTPHost string `short:"a" long:"http-host" description:"HTTP server hostname to bind on"`
 }
 
 // Execute of StartCmd will start running the web server
 func (cmd *StartCmd) Execute(args []string) (err error) {
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-
-	client, err := setupMQTTClient(cmd)
-	if err != nil {
-		return err
+	// Confirm the HTTPHost and HTTPPort settings
+	if cmd.HTTPPort == 0 {
+		return fmt.Errorf("invalid port number")
 	}
-	defer client.Disconnect(0)
 
-	// Before starting the server, check if we need to register the MQTT server with Edgex Distro
-	if cmd.EdgeXRegisterMQTT {
+	// Before starting the server, check if we need to register this REST server with Edgex Distro
+	if cmd.EdgeXRegisterRESTClient {
 		edgexRegistrationEndpoint := fmt.Sprintf("http://%s:%d/api/v1/registration", cmd.EdgeXExportDistroHost, cmd.EdgeXExportDistroPort)
-		// since we're creating a new MQTT registration, we need to check if we should delete the current registration if it exists
+		// since we're creating a new REST registration, we need to check if we should delete the current registration if it exists
 		if cmd.EdgexDeleteRegistration {
 			// make a new client + DELETE request against the registration endpoint with the registration name directory
 			client := &http.Client{}
@@ -128,8 +104,11 @@ func (cmd *StartCmd) Execute(args []string) (err error) {
 			}
 		}
 
-		// Format the registration json with the mqtt host and the mqtt port
-		registerJSON := fmt.Sprintf(edgeXCreateMQTTRegistrationJSON, cmd.MQTTHost, cmd.MQTTPort)
+		// because the export-distro service needs to make a network connection to this machine, and may be on a different machine than this one
+		// we need to check to make sure that if the host for this server was specified as
+
+		// Format the registration json with this server's http host and port
+		registerJSON := fmt.Sprintf(edgeXCreateRESTRegistrationJSON, cmd.HTTPHost, cmd.HTTPPort)
 		// POST the request to export-client's registation endpoint with the formatted JSON as the body
 		res, err := http.Post(
 			edgexRegistrationEndpoint,
@@ -145,13 +124,13 @@ func (cmd *StartCmd) Execute(args []string) (err error) {
 			if err != nil {
 				return err
 			}
-			return fmt.Errorf("failed to register mqtt broker with export-distro (status code : %d): %s", res.StatusCode, body)
+			return fmt.Errorf("failed to register REST endpoint with export-distro (status code : %d): %s", res.StatusCode, body)
 		}
 	}
 
-	// Waits for signals
-	<-c
-	return err
+	// start the HTTP server
+	http.HandleFunc("/edgex", readingData)
+	return http.ListenAndServe(fmt.Sprintf("%s:%d", cmd.HTTPHost, cmd.HTTPPort), nil)
 }
 
 func genNewClientID() string {
@@ -159,8 +138,38 @@ func genNewClientID() string {
 	return "unique"
 }
 
+func readingData(w http.ResponseWriter, req *http.Request) {
+	// make sure that the method is a post
+	if strings.ToUpper(req.Method) != "POST" {
+		// error only post is supported, add Allow header with just POST
+		// and add MethodNotAllowed response code
+		w.Header().Add("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, err := w.Write([]byte("invalid request, only POST supported"))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error writing error response")
+		}
+		return
+	}
+
+	// attempt to decode the body as an event JSON
+	var event models.Event
+	err := json.NewDecoder(req.Body).Decode(&event)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// now that we have the event, in another go routine
+	// we send the event's readings to influxdb
+	// this is because we don't need to block this http request with the result of sending
+	// to influx
+	go sendEventToInflux(event)
+	w.WriteHeader(http.StatusOK)
+}
+
 // TODO: this should parse the message from edgex and save it into an in memory database, etc.
-func onMessageReceived(client MQTT.Client, message MQTT.Message) {
+func sendEventToInflux(event models.Event) {
 	// Make a new HTTP client connection to influxdb
 	influxClient, err := influx.NewHTTPClient(influx.HTTPConfig{
 		Addr: "http://localhost:8086",
@@ -177,65 +186,59 @@ func onMessageReceived(client MQTT.Client, message MQTT.Message) {
 		Precision: "us",
 	})
 
-	var event models.Event
-	err = json.Unmarshal(message.Payload(), &event)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error decoding message: %+v\n", err)
-	} else {
-		// Add all readings into batch influxdb points
-		for _, reading := range event.Readings {
+	// Add all readings into batch influxdb points
+	for _, reading := range event.Readings {
 
-			// Make the map of metadata for this reading (i.e. "tags" in influxdb parlance)
-			tags := map[string]string{
-			// "id": reading.Id.Hex(),
-			}
-
-			// Make a map for the reading values (i.e "fields" in influxdb parlance) and
-			// dynamically attempt to parse the type of value for this reading - edgex returns
-			// all reading types as strings, so we have to do a little bit of guesswork to
-			// figure out the actual underlying value type
-			// For the field name, we use the reading name here
-			fields := make(map[string]interface{})
-			readingType, boolVal, floatVal, intVal := parseValueType(reading.Value)
-			switch readingType {
-			case BoolType:
-				fields[reading.Name] = boolVal
-			case IntType:
-				fields[reading.Name] = intVal
-			case FloatType:
-				fields[reading.Name] = floatVal
-			case StringType:
-				fields[reading.Name] = reading.Value
-			}
-
-			// Calculate the unix time from the origin time in the reading
-			unixTime := float64(reading.Origin) / 1000.0
-			unixTimeSec := math.Floor(unixTime)
-			unixTimeNSec := int64((unixTime - unixTimeSec) * 1000000000.0)
-
-			// Make the reading point for this device
-			pt, err := influx.NewPoint(
-				reading.Device,
-				tags,
-				fields,
-				// need to make sure the Time value returned is in UTC - but note we don't have to convert it before hand
-				// because Unix time is always in UTC, but time.Time is in the local timezone
-				time.Unix(int64(unixTimeSec), unixTimeNSec),
-			)
-			if err != nil {
-				// TODO : send error via channel
-				fmt.Fprintf(os.Stderr, "error creating reading point: %+v\n", err)
-			}
-
-			// Add it to the batch set
-			bp.AddPoint(pt)
+		// Make the map of metadata for this reading (i.e. "tags" in influxdb parlance)
+		tags := map[string]string{
+		// "id": reading.Id.Hex(),
 		}
 
-		// finally write all these points out to influx
-		err = influxClient.Write(bp)
+		// Make a map for the reading values (i.e "fields" in influxdb parlance) and
+		// dynamically attempt to parse the type of value for this reading - edgex returns
+		// all reading types as strings, so we have to do a little bit of guesswork to
+		// figure out the actual underlying value type
+		// For the field name, we use the reading name here
+		fields := make(map[string]interface{})
+		readingType, boolVal, floatVal, intVal := parseValueType(reading.Value)
+		switch readingType {
+		case BoolType:
+			fields[reading.Name] = boolVal
+		case IntType:
+			fields[reading.Name] = intVal
+		case FloatType:
+			fields[reading.Name] = floatVal
+		case StringType:
+			fields[reading.Name] = reading.Value
+		}
+
+		// Calculate the unix time from the origin time in the reading
+		unixTime := float64(reading.Origin) / 1000.0
+		unixTimeSec := math.Floor(unixTime)
+		unixTimeNSec := int64((unixTime - unixTimeSec) * 1000000000.0)
+
+		// Make the reading point for this device
+		pt, err := influx.NewPoint(
+			reading.Device,
+			tags,
+			fields,
+			// need to make sure the Time value returned is in UTC - but note we don't have to convert it before hand
+			// because Unix time is always in UTC, but time.Time is in the local timezone
+			time.Unix(int64(unixTimeSec), unixTimeNSec),
+		)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "error writing points to influx: %+v\n", err)
+			// TODO : send error via channel
+			fmt.Fprintf(os.Stderr, "error creating reading point: %+v\n", err)
 		}
+
+		// Add it to the batch set
+		bp.AddPoint(pt)
+	}
+
+	// finally write all these points out to influx
+	err = influxClient.Write(bp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error writing points to influx: %+v\n", err)
 	}
 }
 
@@ -272,16 +275,6 @@ func parseValueType(valueStr string) (typeStr DataValueType, boolVal bool, float
 	// if we get here, it's not any scalar numeric value, so just assume it's meant as a string
 	typeStr = StringType
 	return
-}
-
-// mqtt disconnect callback
-func onDisconnect(client MQTT.Client, err error) {
-	fmt.Printf("client disconnected: %+v\n", err)
-}
-
-// mqtt connect callback
-func onConnect(client MQTT.Client) {
-	fmt.Println("client connected")
 }
 
 // command parser
