a4ee94d409424fa5041e2fa61e67206b43cf319d
Ian Johnson <ian.johnson@canonical.com>
2018-04-10 11:27:06 -0500

    add config package
    This is used for loading config values from a config file rather than through command line options.

---
+++ config/config.go
@@ -0,0 +1,108 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	toml "github.com/pelletier/go-toml"
+)
+
+// ServerConfig holds all of the config values
+type ServerConfig struct {
+	HTTPConfig struct {
+		Port int    `toml:"port"`
+		Host string `toml:"host"`
+	} `toml:"http"`
+	InfluxConfig struct {
+		Port        int    `toml:"port"`
+		Host        string `toml:"host"`
+		DBName      string `toml:"dbname"`
+		DBPrecision string `toml:"dbprecision"`
+	} `toml:"influx"`
+	EdgeXConfig struct {
+		RegisterRESTClient bool   `toml:"register"`
+		CleanRegistration  bool   `toml:"clean-register"`
+		ExportDistroHost   string `toml:"exporthost"`
+		ExportDistroPort   int    `toml:"exportport"`
+	} `toml:"edgex"`
+}
+
+// Config is the current server config
+var Config *ServerConfig
+
+// LoadConfig will read in the file, loading the config, and perform validation on the config
+func LoadConfig(file string) error {
+	// open the file for reading
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	// make a new decoder with the file
+	// and decode the file
+	err = toml.NewDecoder(f).Decode(Config)
+	if err != nil {
+		return err
+	}
+
+	// validate the config
+	return checkConfig(Config)
+}
+
+// checks that the precision for the database is correctly specified
+func validDBPrecision(prec string) bool {
+	switch prec {
+	case "ns",
+		"us",
+		"ms",
+		"s",
+		"":
+		return true
+	default:
+		return false
+	}
+}
+
+// checks various properties in the config to make sure they're usable
+func checkConfig(cfg *ServerConfig) error {
+	switch {
+	// check that ports are greater than 0
+	case cfg.HTTPConfig.Port < 1:
+		return fmt.Errorf("http port %d is invalid", cfg.HTTPConfig.Port)
+	case cfg.InfluxConfig.Port < 1:
+		return fmt.Errorf("influx port %d is invalid", cfg.InfluxConfig.Port)
+	// only check the edgex port if we need to register the server as a REST client
+	case cfg.EdgeXConfig.RegisterRESTClient && cfg.EdgeXConfig.Port < 1:
+		return fmt.Errorf("edgex export-distro port %d is invalid", cfg.EdgeXConfig.Port)
+	// check the database name
+	case cfg.InfluxConfig.DBName == "":
+		return fmt.Errorf("influx dbname %s is invalid", cfg.InfluxConfig.DBName)
+	// check the database precision
+	case validDBPrecision(cfg.InfluxConfig.DBPrecision):
+		return fmt.Errorf("influx db precision %s is invalid", cfg.InfluxConfig.DBName)
+	default:
+		return nil
+	}
+}
+
+// default values for the config
+func defaultConfig() *ServerConfig {
+	return &ServerConfig{
+		HTTPConfig{
+			Port: 8080,
+			Host: "",
+		},
+		InfluxConfig{
+			Host:        "localhost",
+			Port:        8086,
+			DBName:      "edgex",
+			DBPrecision: "ns",
+		},
+		EdgeXConfig{
+			RegisterRESTClient: true,
+			CleanRegistration:  true,
+			ExportDistroHost:   "localhost",
+			ExportDistroPort:   "48071",
+		},
+	}
+}
