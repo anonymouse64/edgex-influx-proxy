a9f5eee1dbb1e7289358361c0520ca08c38b7fcf
Ian Johnson <ian.johnson@canonical.com>
2019-04-08 18:04:16 -0500

    runserver.sh: add quotes to fix shellcheck warnings
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -2,7 +2,7 @@
 set -e
 
 # check if the config file exists - if not then create a default one
-$SNAP/bin/influxproxy -c $SNAP_DATA/config.toml config check --write-new
+"$SNAP/bin/influxproxy" -c "$SNAP_DATA/config.toml" config check --write-new
 
 # now actually run the server
-$SNAP/bin/influxproxy -c $SNAP_DATA/config.toml start 
+"$SNAP/bin/influxproxy" -c "$SNAP_DATA/config.toml" start
