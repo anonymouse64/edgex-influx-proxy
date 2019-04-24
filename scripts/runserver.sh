7534b2191825bbd9e8e8c256f8f6a206750f507f
Ian Johnson <ian.johnson@canonical.com>
2019-04-24 17:57:37 -0500

    scripts/runserver.sh: sleep if the server exits
    This is to prevent systemd from giving up on starting us if edgex isn't
    ready yet - we want to give edgex some time to start up.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
 # check if the config file exists - if not then create a default one
 "$SNAP/bin/influxproxy" -c "$SNAP_DATA/config.toml" config check --write-new
 
-# now actually run the server
-"$SNAP/bin/influxproxy" --debug -c "$SNAP_DATA/config.toml" start
+# now actually run the server - if it fails to run then sleep for 5 seconds
+# to attempt to wait for edgexfoundry to come online
+if ! "$SNAP/bin/influxproxy" --debug -c "$SNAP_DATA/config.toml" start ; then
+    sleep 5
+fi
